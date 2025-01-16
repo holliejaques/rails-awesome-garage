e9460e395103a9e07e818914269c02cbd3e82f06
Hollie Jaques <holliejaques@gmail.com>
2025-01-16 19:02:21 +0700

    created seed file

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :owner
-  has_many :reviews, :favourites, dependent: :destroy
+  has_many :reviews
+  has_many :favourites
   validates :model, presence: true
   validates :brand, presence: true
   validates :year, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,64 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning database...'
+
+Car.destroy_all
+Owner.destroy_all
+
+puts 'Creating owners...'
+
+toad = Owner.create!(
+  nickname: 'Toad'
+)
+
+puts 'Created Toad'
+
+peach = Owner.create!(
+  nickname: 'Peach'
+)
+
+puts 'Created Peach'
+
+donkey_kong = Owner.create!(
+  nickname: 'Donkey Kong'
+)
+
+puts 'Donkey Kong'
+
+puts "Finsihed! Created #{Owner.count} owners!"
+
+puts 'Creating vehicles...'
+
+Car.create!(
+  brand: 'Mario Kart DS',
+  model: 'Monster Truck',
+  year: 2003,
+  fuel: 'Coins',
+  owner: toad
+)
+
+puts 'Created Tiny Titan'
+
+Car.create!(
+  brand: 'Super Mario Kart',
+  model: 'Bike',
+  year: 1992,
+  fuel: 'Mushrooms',
+  owner: peach
+)
+
+puts 'Created Mach 8'
+
+Car.create!(
+  brand: 'Mario Kart 8 Deluxe',
+  model: 'Kart',
+  year: 1996,
+  fuel: 'Power Slides',
+  owner: donkey_kong
+)
+
+puts 'Created Barrel Kart'
+
+puts "Finished! Created #{Car.count} vehicles!"
