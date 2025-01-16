7c1f032dfe6e9c9f2b72ed50de75c5d0278dc515
Hollie Jaques <holliejaques@gmail.com>
2025-01-16 17:55:34 +0700

    added model associations and validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :owner
+  has_many :reviews, :favourites, dependent: :destroy
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :year, presence: true
+  validates :fuel, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :car
+  validates :comment, presence: true, length: { minimum: 6, too_short: 'must have atleast 6 characters' }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
